8364db795e67008c96664d51c630bddb79aa9318
BT_1N3_12 <jalam6556@bths.edu>
2018-10-05 11:22:25 -0400

    Classroom done.

--- src/com/company/Classroom.java
+++ src/com/company/Classroom.java
@@ -1,4 +1,24 @@
 package com.company;
 
 public class Classroom {
+    Student[] students;
+    Teacher teacher;
+    public Classroom (Student[] students, Teacher teacher){
+        super("Classroom");
+        this.students = students;
+        this.teacher = teacher;
+    }
+    public String getSubject(){
+        return "Math";
+    }
+    public double classAverage(){
+        double total = 0;
+        double num = 0;
+        for(int i = 0; i < this.students.length; i++){
+            total += this.students[i].getGPA();
+            num++;
+        }
+        double avg = total/num;
+        return avg;
+    }
 }

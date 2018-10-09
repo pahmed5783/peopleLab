8bc7285f04c74bb29934abf564720232ceb2df00
BT_1N3_10 <pahmed5783@bths.edu>
2018-10-09 11:03:49 -0400

    teacher method

--- src/com/company/Teacher.java
+++ src/com/company/Teacher.java
@@ -1,4 +1,20 @@
 package com.company;
 
-public class Teacher {
+public class Teacher extends Person {
+    private String Subject;
+    private String Title;
+
+    public Teacher(String Subject, String Title, String firstName, String familyName) {
+        super(firstName, familyName);
+        this.Subject = Subject;
+        this.Title = Title;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public String toString() {
+        return this.Title + "." + " " + this.getFamilyName();
+    }
 }

18f7ddf0e22f0fb8ec6219dd2561022651865334
cchau3846 <43016845+cchau3846@users.noreply.github.com>
2018-11-08 22:57:40 -0500

    finish

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -1,16 +1,36 @@
 package com.company;
 
-public class Runner{
-    static String teacher = "Mr.Folwell";
-    static String firstName[] = {"Christina", "Bob", "Parker", "James", "Lily"};
-    static String familyName[] = {"Chau", "Gates", "Ahmed", "Lui", "Huang"};
-    static double gpa[] = {90, 87, 95, 89, 79};
-    static String color[]= {"blue", "purple", "black", "red", "green"};
-    static String subject[] = {"Physics", "Chemistry","Calculus", "Algebra", "Economics"};
+public class Runner {
+    // Fields
+    private static String[] firstNames = new String[]{"Abir","Madhi","Parker","Nusrat","Vincent","Jacob","Christina"};
+    private static String[] familyNames = new String[]{"Ahmed","Ahmed","Ahmed","Alam","Bond-Moore","Cedar","Chau","Chen","Chen","Cho","Gyawu","Jack","Kang","Ko","Korsunsky","Lay"};
 
-    public static void main(String[] args){
-            int num = (int)(Math.random()*6) - 1;
-            String student = firstName[num] +" "+ familyName[num]+ " has GPA of "+ gpa[num]+", likes the color " + color[num] + ", and has " + subject[num] + " with " + teacher;
-            System.out.println(student);
+    // Methods
+    public static void main(String[] args) {
+        int classSize = 36;
+
+        Teacher ourTeacher = new Teacher("Nathan","Folwell","AP Java","Mr");
+
+        Student[] ourStudents = new Student[classSize];
+        for (int i = 0; i < classSize; i ++) {
+            ourStudents[i] = randomStudent();
+        }
+
+        Classroom ourClassroom = new Classroom(ourStudents, ourTeacher);
+
+        System.out.println(ourClassroom.printClass());
+        System.out.println("\nClass Average: " + ourClassroom.classAverage());
+    }
+
+    /**
+     * Builds a random student with a random first name, last name, and GPA.
+     * @return Student
+     */
+    public static Student randomStudent() {
+        String randomFirstName = firstNames[(int)(Math.random() * firstNames.length)];
+        String randomFamilyName = familyNames[(int)(Math.random() * familyNames.length)];
+        double randomGPA = Math.round((Math.random() * 3 + 1) * 100) / 100.0;
+
+        return new Student(randomFirstName, randomFamilyName, randomGPA,12,"Software Engineering");
     }
 }

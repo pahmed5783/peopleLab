7d84a636f82b09f5abf0e4781f561984ed07c208
cchau3846 <43016845+cchau3846@users.noreply.github.com>
2018-11-08 22:55:07 -0500

    Update Classroom.java
    finish

--- src/com/company/Classroom.java
+++ src/com/company/Classroom.java
@@ -1,38 +1,74 @@
 package com.company;
 
 public class Classroom {
-    static Student[] students;
-    Student [][] seatingChart = new Student[6][6];
-    Teacher teacher;
-    public Classroom(Student[] student, Teacher teacher){
-        this.students = student;
-        this.teacher= teacher;
+    // Fields
+    private Student[] students;
+    private Teacher teacher;
+    private Student[][] seatingChart = new Student[6][6];
+
+    // Constructor
+    public Classroom(Student[] students, Teacher teacher) {
+        this.students = students;
+        this.teacher = teacher;
         fillSeats();
     }
 
-    public Student[][] fillSeats() {
-        for(int i = 0; i < seatingChart.length; i++){
-            if(i < 6){
-                seatingChart[0][i] = Classroom.students[i];
-            } else {
-                seatingChart[1][i] = Classroom.students[i];
-            }
-        }
-        return seatingChart;
+    // Methods
+
+    // Getters
+    public Student[] getStudents() {
+        return students;
+    }
+    public Teacher getTeacher() {
+        return teacher;
     }
 
-    public String getSubject(){
-        return "Math";
+    // Setters
+    public void setStudents(Student[] students) {
+        this.students = students;
     }
-    public double classAverage(){
-        double total = 0;
-        double num = 0;
-        for(int i = 0; i < this.students.length; i++){
-            total += this.students[i].getGPA();
-            num++;
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    /**
+     * Adds up the GPAs of all students and divides by # of students.
+     * @return average of GPAs of students
+     */
+
+    public double classAverage() {
+        double sum = 0;
+
+        for (Student cur : students)
+            sum += cur.getGPA();
+
+        return sum / students.length;
+    }
+
+    /**
+     * @return class Teacher, then subject, then all students in the class.
+     */
+
+    public String printClass() {
+        String studentString = "";
+
+        for (int i = 0; i < students.length; i++) {
+            studentString += "\n" + (i + 1) + ". " + students[i].toString();
         }
-        double avg = total/num;
-        return avg;
-        //oofff
+
+        return "Teacher: " + teacher.toString() +
+                "\nSubject: " + teacher.getSubject() +
+                "\nStudents: " + studentString;
     }
+
+    private void fillSeats() {
+        int idx = 0;
+        for (Student[] studentArr : seatingChart) {
+            for (int i = 0; i < studentArr.length; i++) {
+                studentArr[i] = students[idx];
+                idx++;
+            }
+        }
+    }
+
 }
